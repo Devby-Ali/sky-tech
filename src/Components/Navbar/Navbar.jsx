6497f58023fd64bc62ab5d6274ade710aafc2180
Ali Bakhshi <alibakhshi962002@gmail.com>
2024-11-28 02:24:21 +0330

    refactor Navbar Component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -70,9 +70,97 @@ export default function Navbar() {
           <span className="text-2xl">{authContext.userInfos.name}</span>
         </Card>
       </Collapse>
-      <header className="fixed hidden lg:flex top-7 right-0 left-0 z-50 justify-between items-center w-[98%] lg:w-[95%] h-32 2xl:h-36 mx-auto rounded-4xl bg-gradient-to-tr from-lightishBlue-400/50 via-darkBox/80 via-60% to-lightishBlue-400/50 backdrop-blur-[4px]">
-        <div className="w-full">
-          <div className="flex items-center justify-between px-12 xl:px-24 py-4">
+
+      <div
+        className={`nav fixed flex-col top-0 bottom-0 w-[263px] min-h-screen px-7 bg-white dark:bg-darkColor overflow-y-auto transition-all z-50 ${
+          navOpen ? "right-0" : "-right-[263px]"
+        }`}
+      >
+        <div className="flex items-center justify-between gap-x-14 h-20 mb-3 text-darkColor dark:text-white px-1 py-16">
+          <Link
+            to={"/"}
+            onClick={navOpenHandler}
+            className="text-light-blue-600 dark:text-light-blue-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="65"
+              height="65"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-brand-my-oppo"
+              viewBox="0 0 24 24"
+            >
+              <path stroke="none" d="M0 0h24v24H0z"></path>
+              <path d="M18.316 5H5.684L2.266 9.019a1.09 1.09 0 0 0 .019 1.447L11.999 21l9.715-10.49a1.09 1.09 0 0 0 .024-1.444z"></path>
+              <path d="m9 11 3 3 3-3"></path>
+            </svg>
+          </Link>
+          <div className="flex items-center gap-x-6 text-4xl text-blue-gray-500 dark:text-white">
+            <div
+              className="flex-center p-5 rounded-full toggle-theme cursor-pointer bg-blue-gray-50 dark:bg-darkBox"
+              onClick={() => themeHandler()}
+            >
+              <IoSunnyOutline className="hidden dark:inline-block" />
+
+              <BsMoon className="inline-block dark:hidden" />
+            </div>
+            <div
+              className="flex-center p-5 rounded-full cursor-pointer bg-blue-gray-50 dark:bg-darkBox"
+              onClick={navOpenHandler}
+            >
+              <IoIosArrowForward />
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-px bg-gray-300 dark:bg-white/10"></div>
+        <form action="https://" method="get">
+          <label className="relative py-5 mt-10 block bg-gray-100 text-gray-700 dark:bg-white/5 dark:text-white text-2xl rounded-tl-4xl rounded-br-4xl">
+            <input
+              className="bg-transparent pr-4 pl-12 w-72 h-full outline-none"
+              type="text"
+              placeholder="چیو میخوای یاد بگیری؟"
+              name="s"
+            />
+            <button
+              className="absolute left-4 top-0 bottom-0 my-auto text-gray-700 dark:text-white"
+              type="submit"
+              role="button"
+            >
+              <div className="text-4xl">
+                <PiMagnifyingGlass />
+              </div>
+            </button>
+          </label>
+        </form>
+
+        <ul className="child:transition-all child:pr-2.5 space-y-12 mt-12 text-darkColor dark:text-white ">
+          {allMenus.map((menu) => (
+            <li key={menu._id} className="flex items-center justify-between">
+              <Link to={menu.href} className="flex items-center gap-x-2">
+                {menu.title}
+              </Link>
+              <IoIosArrowBack className="text-3xl" />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <header
+        className="lg:fixed lg:flex lg:top-7 lg:right-0 lg:left-0 z-40 justify-between items-center w-full lg:w-[95%] h-[8.5rem] md:h-40 2xl:h-36 lg:mx-auto lg:rounded-4xl shadow-lg bg-gradient-to-tr from-light-blue-900 via-darkBox to-light-blue-900 lg:from-lightishBlue-400/50 lg:via-darkBox/80 via-60% lg:to-lightishBlue-400/50 lg:backdrop-blur-[4px]"
+      >
+        <div className="w-full h-full">
+          <div className="h-full flex items-center justify-between px-12 xl:px-24 py-4">
+            <div
+              className="lg:hidden bg-gray-300 dark:bg-white/10 text-gray-500 dark:text-white cursor-pointer text-5xl p-4 rounded-full"
+              onClick={navOpenHandler}
+            >
+              <IoMenu />
+            </div>
+
             <div className="flex gap-x-14">
               <Link to={"/"} className="text-light-blue-500">
                 <svg
@@ -93,7 +181,7 @@ export default function Navbar() {
                 </svg>
               </Link>
 
-              <ul className="flex gap-x-12 text-white text-[1.7rem]">
+              <ul className="hidden lg:flex gap-x-12 text-white text-[1.7rem]">
                 <li className="main-header__item flex-center relative">
                   <Link to={"/"} className="flex-center text-da">
                     صفحه اصلی
@@ -135,7 +223,7 @@ export default function Navbar() {
 
             <div className="flex items-center">
               <div
-                className="flex-center w-16 h-16 bg-lightishBlue-500 text-white text-4xl rounded-lg toggle-theme cursor-pointer"
+                className="hidden lg:flex-center w-16 h-16 bg-lightishBlue-500 text-white text-4xl rounded-lg toggle-theme cursor-pointer"
                 onClick={() => themeHandler()}
               >
                 <IoSunnyOutline className="hidden dark:inline-block w-8 h-8" />
@@ -171,134 +259,6 @@ export default function Navbar() {
           </div>
         </div>
       </header>
-
-      {/* Mobile Menu */}
-
-      {/* <div className="flex lg:hidden fixed w-full z-30 items-center justify-between h-32 px-4 bg-gradient-to-tr from-purple-700/80 via-teal-800/80 to-lightishBlue-600/80 backdrop-blur-[4px]"> */}
-      <div className="flex items-center justify-between lg:hidden fixed mx-auto w-full h-[8.5rem] z-30 px-6 lg:px-14 bg-white dark:bg-darkBox shadow-lg">
-        <div
-          className="bg-gray-300 dark:bg-white/10 text-gray-500 dark:text-white cursor-pointer text-5xl p-4 rounded-full"
-          onClick={navOpenHandler}
-        >
-          <IoMenu />
-        </div>
-
-        <div
-          className={`nav fixed flex-col top-0 bottom-0 w-[263px] min-h-screen px-7 bg-white dark:bg-darkColor overflow-y-auto transition-all ${
-            navOpen ? "right-0" : "-right-[263px]"
-          }`}
-        >
-          <div className="flex items-center justify-between gap-x-14 h-20 mb-3 text-darkColor dark:text-white px-1 py-16">
-            <Link
-              to={"/"}
-              onClick={navOpenHandler}
-              className="text-light-blue-600 dark:text-light-blue-500"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="65"
-                height="65"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-brand-my-oppo"
-                viewBox="0 0 24 24"
-              >
-                <path stroke="none" d="M0 0h24v24H0z"></path>
-                <path d="M18.316 5H5.684L2.266 9.019a1.09 1.09 0 0 0 .019 1.447L11.999 21l9.715-10.49a1.09 1.09 0 0 0 .024-1.444z"></path>
-                <path d="m9 11 3 3 3-3"></path>
-              </svg>
-            </Link>
-            <div className="flex items-center gap-x-6 text-4xl text-blue-gray-500 dark:text-white">
-              <div
-                className="flex-center p-5 rounded-full toggle-theme cursor-pointer bg-blue-gray-50 dark:bg-darkBox"
-                onClick={() => themeHandler()}
-              >
-                <IoSunnyOutline className="hidden dark:inline-block" />
-
-                <BsMoon className="inline-block dark:hidden" />
-              </div>
-              <div
-                className="flex-center p-5 rounded-full cursor-pointer bg-blue-gray-50 dark:bg-darkBox"
-                onClick={navOpenHandler}
-              >
-                <IoIosArrowForward />
-              </div>
-            </div>
-          </div>
-          <div className="w-full h-px bg-gray-300 dark:bg-white/10"></div>
-          <form action="https://" method="get">
-            <label className="relative py-5 mt-10 block bg-gray-100 text-gray-700 dark:bg-white/5 dark:text-white text-2xl rounded-tl-4xl rounded-br-4xl">
-              <input
-                className="bg-transparent pr-4 pl-12 w-72 h-full outline-none"
-                type="text"
-                placeholder="چیو میخوای یاد بگیری؟"
-                name="s"
-              />
-              <button
-                className="absolute left-4 top-0 bottom-0 my-auto text-gray-700 dark:text-white"
-                type="submit"
-                role="button"
-              >
-                <div className="text-4xl">
-                  <PiMagnifyingGlass />
-                </div>
-              </button>
-            </label>
-          </form>
-
-          <ul className="child:transition-all child:pr-2.5 space-y-12 mt-12 text-darkColor dark:text-white ">
-            {allMenus.map((menu) => (
-              <li key={menu._id} className="flex items-center justify-between">
-                <Link to={menu.href} className="flex items-center gap-x-2">
-                  {menu.title}
-                </Link>
-                <IoIosArrowBack className="text-3xl" />
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="logo">
-          <Link
-            to={"/"}
-            className="text-light-blue-600 dark:text-light-blue-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="65"
-              height="65"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-brand-my-oppo"
-              viewBox="0 0 24 24"
-            >
-              <path stroke="none" d="M0 0h24v24H0z"></path>
-              <path d="M18.316 5H5.684L2.266 9.019a1.09 1.09 0 0 0 .019 1.447L11.999 21l9.715-10.49a1.09 1.09 0 0 0 .024-1.444z"></path>
-              <path d="m9 11 3 3 3-3"></path>
-            </svg>
-          </Link>
-        </div>
-        <div className="flex gap-x-4">
-          <Link
-            to="#"
-            className="bg-gray-300 dark:bg-white/10 text-gray-500 dark:text-white cursor-pointer text-5xl p-4 rounded-full"
-          >
-            <RiShoppingCartFill />
-          </Link>
-          <Link
-            to={"#"}
-            className="bg-gray-300 dark:bg-white/10 text-gray-500 dark:text-white cursor-pointer text-5xl p-4 rounded-full"
-          >
-            <CgProfile />
-          </Link>
-        </div>
-      </div>
       <div
         onClick={overlayOnClick}
         className={overlay ? "overlay overlay--visible" : "overlay"}
