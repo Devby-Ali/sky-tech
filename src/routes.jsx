121ebd4b275b352dfd8130e6e8deeaf904334699
Ali Bakhshi <alibakhshi962002@gmail.com>
2024-12-24 19:09:17 +0330

    coding adminPanel

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,12 @@ import Register from "./pages/Register/Register";
 import Contact from "./pages/Contact/Contact";
 import Search from "./pages/Search/Search";
 
+import AdminPanel from "./pages/AdminPanel/index";
+import Users from "./pages/AdminPanel/Users/Users";
+import AdminCourses from "./pages/AdminPanel/Courses/Courses";
+import Menus from "./pages/AdminPanel/Menus/Menus";
+import AdminArticles from "./pages/AdminPanel/Articles/Articles";
+
 const routes = [
   { path: "/", element: <Index /> },
   { path: "/course-info/:courseName", element: <CourseInfo /> },
@@ -20,6 +26,17 @@ const routes = [
   { path: "/register", element: <Register /> },
   { path: "/contact", element: <Contact /> },
   { path: "/search/:value", element: <Search /> },
+
+  {
+    path: "/p-admin/*",
+    element: <AdminPanel />,
+    children: [
+      { path: "users", element: <Users /> },
+      { path: "courses", element: <AdminCourses /> },
+      { path: "menus", element: <Menus /> },
+      { path: "articles", element: <AdminArticles /> },
+    ],
+  },
 ];
 
 export default routes;
