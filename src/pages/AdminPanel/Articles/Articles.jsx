6628f0e33749a81e099e49a16a32a5724fca57f2
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-01-02 14:36:09 +0330

    show articles list pAdmin

--- src/pages/AdminPanel/Articles/Articles.jsx
+++ src/pages/AdminPanel/Articles/Articles.jsx
@@ -1,7 +1,125 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from './../../../Components/AdminPanel/DataTable/DataTable'
+import { Card, Typography } from '@material-tailwind/react';
+
+const TABLE_HEAD = [
+  "شناسه",
+  "عنوان",
+  "لینک",
+  "نویسنده",
+  "ویرایش",
+  "حذف",
+];
 
 export default function Articles() {
+
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    getAllArticles();
+  }, [])
+
+  function getAllArticles() {
+    fetch('http://localhost:4000/v1/articles')
+    .then(res => res.json())
+    .then(allArticles => {
+      console.log(allArticles);
+      setArticles(allArticles)
+    })
+  }
+
   return (
-    <div>Articles</div>
+    <>
+          <DataTable title="مقاله‌ها">
+        <Card className="h-full w-full rounded-md overflow-scroll px-6">
+          <table className="w-full min-w-max table-auto text-center">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b-4 border-gray-400 pb-10 pt-12"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="text-4xl font-EstedadBold leading-none"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {articles.map((article, index) => {
+                const isLast = index === articles.length - 1;
+                const classes = isLast
+                  ? "py-6"
+                  : "py-6 border-b border-gray-400";
+
+                return (
+                  <tr key={article.title} className="hover:bg-gray-50">
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="text-2xl font-EstedadBold"
+                      >
+                        {index + 1}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        className="text-darkBox text-[1.6rem] font-EstedadLight"
+                      >
+                        {article.title}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        className="text-darkBox text-[1.6rem] font-EstedadLight"
+                      >
+                        {article.shortName}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        className="text-darkBox text-[1.6rem] font-EstedadLight"
+                      >
+                        {article.creator.name}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        className="text-darkBox text-[1.6rem] font-EstedadLight"
+                      >
+                        <button type="button">ویرایش</button>
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        className="text-darkBox text-[1.6rem] font-EstedadLight"
+                      >
+                        <button
+                          type="button"
+                        >
+                          حذف
+                        </button>
+                      </Typography>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Card>
+      </DataTable>
+    </>
   )
 }
