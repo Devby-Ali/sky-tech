8c7965bbdfc446af55f6129ece7bec56e5fb0000
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-01-03 22:28:28 +0330

    prepare create session form

---
+++ src/pages/AdminPanel/Sessions/Sessions.jsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "../../../hooks/useForm";
+import Input from "../../../Components/Form/Input";
+import { minValidator } from "../../../validators/rules";
+import Button from "../../../Components/Form/Button";
+
+export default function Sessions() {
+  const [courses, setCourses] = useState([]);
+  const [sessionCourse, setSessionCourse] = useState("-1");
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      time: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:4000/v1/courses")
+      .then((res) => res.json())
+      .then((allCourses) => {
+        console.log(allCourses);
+        setCourses(allCourses);
+      });
+  }, []);
+
+  return (
+    <>
+
+          <section className="flex-center overflow-hidden mt-12">
+            <div className="mx-auto flex flex-col items-center w-min">
+              <div className="flex flex-col items-center text-darkColor dark:text-white bg-light-blue-500/20 dark:bg-[#2f3749]/40 backdrop-blur-[4px] px-10 pb-10 pt-8 rounded-3xl w-[33rem] sm:w-[37rem] lg:w-[40rem] z-10">
+                <span className="block font-EstedadMedium text-4xl mb-14 mt-4">
+                افزودن جلسه جدید
+                </span>
+                <form action="#" className="w-full flex flex-col gap-y-8">
+                <div className="h-20 flex items-center justify-between px-4 bg-white dark:bg-[#333c4c] rounded-2xl">
+                  <Input
+                    id="name"
+                    className="bg-transparent outline-none"
+                    type="text"
+                    placeholder="عنوان"
+                    validations={[minValidator(5)]}
+                    onInputHandler={onInputHandler}
+                  />
+                </div>
+                <div className="min-h-20 flex items-center justify-between px-4 bg-white dark:bg-[#333c4c] rounded-2xl">
+                <Input
+                    id="shortName"
+                    className="bg-transparent outline-none"
+                    type="text"
+                    placeholder="مدت زمان"
+                    validations={[minValidator(5)]}
+                    onInputHandler={onInputHandler}
+                  />
+                </div>
+                <div className="h-20 flex items-center justify-between px-4 bg-white dark:bg-[#333c4c] rounded-2xl">
+                  <div className="flex items-center gap-x-2">
+                    <label className="text-darkColor dark:text-white/70">
+                      دوره
+                    </label>
+                    <select
+                      className="text-darkColor dark:text-white/70 dark:bg-white/10 rounded-md py-2.5 px-1"
+                      onChange={(event) => setSessionCourse(event.target.value)}
+                    >
+                      <option value="-1">مدنظر را انتخاب کنید</option>
+                      {courses.map((course) => (
+                        <option className="text-darkColor text-[1.8rem]" value={course._id} key={course._id}>
+                          {course.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <Button
+                className={`h-20 rounded-4xl ${
+                  formState.isFormValid
+                    ? "bg-light-blue-600/40 hover:bg-light-blue-600/60"
+                    : "bg-[#333c4c]/30"
+                }`}
+                type="submit"
+                disabled={!formState.isFormValid}
+              >
+                <span className="mx-auto">افزودن</span>
+              </Button>
+                </form>
+              </div>
+            </div>
+          </section>
+
+      <div className="container-fluid" id="home-content">
+        <div className="container">
+          <div className="home-title">
+            <span>افزودن جلسه جدید</span>
+          </div>
+          <form className="form">
+            <div className="col-6">
+              <div className="name input">
+                <label className="input-title">عنوان جلسه</label>
+                <Input
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  type="text"
+                  id="title"
+                  validations={[minValidator(5)]}
+                  placeholder="لطفا نام جلسه را وارد کنید..."
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="price input">
+                <label className="input-title">مدت زمان جلسه</label>
+                <Input
+                  element="input"
+                  onInputHandler={onInputHandler}
+                  type="text"
+                  id="time"
+                  validations={[minValidator(5)]}
+                  placeholder="لطفا مدت زمان جلسه را وارد کنید..."
+                />
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="price input">
+                <label className="input-title" style={{ display: "block" }}>
+                  دوره
+                </label>
+                <select
+                  className="select"
+                  onChange={(event) => setSessionCourse(event.target.value)}
+                >
+                  <option value="-1">دوره مدنظر را انتخاب کنید</option>
+                  {courses.map((course) => (
+                    <option value={course._id} key={course._id}>
+                      {course.name}
+                    </option>
+                  ))}
+                </select>
+                <span className="error-message text-danger"></span>
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div className="bottom-form">
+                <div className="submit-btn">
+                  <input type="submit" value="افزودن" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -16,6 +16,7 @@ import Menus from "./pages/AdminPanel/Menus/Menus";
 import AdminArticles from "./pages/AdminPanel/Articles/Articles";
 import AdminCategory from "./pages/AdminPanel/Category/Category";
 import AdminContact from "./pages/AdminPanel/Contact/Contact";
+import Sessions from "./pages/AdminPanel/Sessions/Sessions";
 
 const routes = [
   { path: "/", element: <Index /> },
@@ -39,6 +40,7 @@ const routes = [
       { path: "articles", element: <AdminArticles /> },
       { path: "category", element: <AdminCategory /> },
       { path: "contacts", element: <AdminContact /> },
+      { path: "sessions", element: <Sessions /> }
     ],
   },
 ];
