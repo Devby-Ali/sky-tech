5ad0ee75e093354b9be3a435a94fe1a99fcf5a0e
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-01-06 22:38:21 +0330

    Dev video section Session Page

--- src/pages/Session/Session.jsx
+++ src/pages/Session/Session.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
 
 export default function Session() {
   const { courseName, sessionID } = useParams();
+  const [session, setSession] = useState({});
+  const [sessions, setSessions] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+
+    fetch(`http://localhost:4000/v1/courses/${courseName}/${sessionID}`, {
+      headers: {
+        Authorization: `Bearer ${
+          JSON.parse(localStorage.getItem("user")).token
+        }`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSession(data.session);
+        setSessions(data.sessions);
+        console.log(data)
+      });
+
+  }, []);
 
   return (
     <>
@@ -14,7 +33,27 @@ export default function Session() {
 
       <main className="max-w-[1920px] mx-auto overflow-x-hidden pt-44 lg:pt-52 2xl:pt-56">
         <div className="container">
-          <div className="aspect-video mt-8 sm:mt-10 overflow-hidden rounded-lg"></div>
+                    <Breadcrumb
+                      links={[
+                        {
+                          id: 2,
+                          title: session.title,
+                          to: `course-info/${courseName}`,
+                        },
+                        {
+                          id: 3,
+                          title: session.title,
+                          to: `course-info/${courseName}`,
+                        },
+                      ]}
+                    />
+          <div className="aspect-video mt-8 sm:mt-10 overflow-hidden rounded-lg">
+          <video
+                className="w-full h-full"
+                controls
+                src={`http://localhost:4000/courses/covers/${session.video}`}
+              ></video>
+          </div>
         </div>
       </main>
 
