a8ad015de71caf88ba955561e7c25d3f1145a2be
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-02-12 20:34:23 +0330

    set Landing in Index Page

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -113,7 +113,7 @@ export default function Landing({ info }) {
   };
 
   return (
-    <section className="relative bg-landing h-[48vh] sm:h-[60vh] lg:h-[72vh] xl:h-[90vh] overflow-hidden">
+    <section className="relative bg-landing pb-28 2xl:pb-44 overflow-hidden mb-14 sm:mb-36 lg:mb-48">
       <div
         className={`nav fixed top-0 bottom-0 w-[263px] min-h-screen px-9 bg-white dark:bg-darkColor overflow-y-auto transition-all z-50 lg:hidden ${
           navOpen ? "right-0" : "-right-[263px]"
@@ -206,7 +206,7 @@ export default function Landing({ info }) {
         </ul>
       </div>
 
-      <header className="md:flex max-w-[140rem] justify-between items-center w-full md:w-[95%] md:mx-auto">
+      <header className="lg:container md:flex justify-between items-center w-full md:mx-auto mb-24 2xl:mb-36">
         <div className="w-full h-full">
           <div className="h-full flex items-center justify-between px-12 py-4 md:py-8">
             <div
@@ -216,12 +216,13 @@ export default function Landing({ info }) {
               <HiBars3 />
             </div>
 
-            <div className="flex gap-x-6 xl:gap-x-14">
-              <Link to={"/"} className="text-light-blue-300 mb-1 2xl:mb-0">
+            <div className="flex gap-x-8 xl:gap-x-14">
+              <Link
+                to={"/"}
+                className="text-light-blue-300 mb-1 2xl:mb-0 size-24 md:size-28"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="65"
-                  height="65"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -232,7 +233,7 @@ export default function Landing({ info }) {
                 </svg>
               </Link>
 
-              <ul className="hidden md:flex gap-x-7 xl:gap-x-12 text-white text-[1.7rem] xl:text-[1.8rem] font-EstedadMedium dark:font-EstedadLight">
+              <ul className="hidden md:flex gap-x-12 xl:gap-x-12 text-white text-[1.7rem] xl:text-[1.8rem] font-EstedadMedium dark:font-EstedadLight">
                 <li className="main-header__item flex-center relative">
                   <Link to={"/"} className="flex-center">
                     صفحه اصلی
@@ -286,7 +287,7 @@ export default function Landing({ info }) {
               </ul>
             </div>
 
-            <div className="flex items-center text-white">
+            <div className="flex gap-x-6 items-center text-white">
               <div
                 className="hidden md:flex-center p-4 text-[2.75rem] rounded-xl toggle-theme cursor-pointer"
                 onClick={() => themeHandler()}
@@ -297,7 +298,7 @@ export default function Landing({ info }) {
 
               <Link
                 to="#"
-                className="flex-center p-4 text-[2.75rem] mr-4 rounded-xl"
+                className="flex-center p-4 text-[2.75rem] rounded-xl"
               >
                 <HiOutlineShoppingBag />
               </Link>
@@ -306,7 +307,7 @@ export default function Landing({ info }) {
                 <>
                   <Link
                     to="#"
-                    className={`relative hidden md:flex-center items-center justify-center p-4 rounded-xl mr-4 transition-all duration-200 ${
+                    className={`relative hidden md:flex-center items-center justify-center p-4 rounded-xl transition-all duration-200 ${
                       openCollapse && "z-40 bg-white/10 dark:bg-white/5"
                     }`}
                     onClick={toggleOpen}
@@ -404,32 +405,34 @@ export default function Landing({ info }) {
           </div>
         </div>
       </header>
-      <div className="flex-center h-[70%] mt-6 sm:mt-16 lg:mt-0">
-        <div className="container text-center text-white">
-          <h2 className="text-3xl sm:text-5xl mb-6 sm:mb-20 font-EstedadBold font-bold">
-            <Typewriter
-              onInit={(typeWriter) => {
-                typeWriter
-                  .typeString("ما به هر قیمتی دوره آموزشی تولید نمی‌کنیم!")
-                  .start()
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .typeString("Sky Tech - آکادمی خصوصی برنامه نویسی")
-                  .start()
-                  .pauseFor(2000);
-              }}
-              options={{
-                loop: true,
-              }}
-            />
-          </h2>
-          <h3 className="text-xl/relaxed xs:text-2xl sm:text-3xl px-10 xs:px-0">
-            با اسکای آکادمی، برنامه نویسی تخصصی رو به صورت حرفه ای یاد بگیر .
-          </h3>
+      <div className="flex-center">
+        <div className="container space-y-16 md:space-y-20 xl:space-y-40 text-center text-white">
+          <div>
+            <h2 className="text-3xl sm:text-5xl mb-6 sm:mb-20 font-EstedadBold font-bold">
+              <Typewriter
+                onInit={(typeWriter) => {
+                  typeWriter
+                    .typeString("ما به هر قیمتی دوره آموزشی تولید نمی‌کنیم!")
+                    .start()
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("Sky Tech - آکادمی خصوصی برنامه نویسی")
+                    .start()
+                    .pauseFor(2000);
+                }}
+                options={{
+                  loop: true,
+                }}
+              />
+            </h2>
+            <h3 className="text-xl/relaxed xs:text-2xl sm:text-3xl px-10 xs:px-0">
+              با اسکای آکادمی، برنامه نویسی تخصصی رو به صورت حرفه ای یاد بگیر .
+            </h3>
+          </div>
           <div className="px-6 md:px-0">
-            <div className="max-w-[700px] mt-16 sm:mt-20 lg:mt-24 mx-auto">
+            <div className="max-w-[700px] mx-auto">
               <form
-                className="flex items-center backdrop-blur-[7px] justify-between border-2 dark:border border-white/15 rounded-md py-4 sm:h-24 pl-4 sm:pl-7 pr-8"
+                className="flex items-center backdrop-blur-[7px] justify-between border dark:border border-white/10 rounded-md py-5 sm:h-[6.5rem] md:h-28 pl-4 sm:pl-7 pr-8"
                 action="#"
               >
                 <Input
@@ -450,7 +453,7 @@ export default function Landing({ info }) {
               </form>
             </div>
           </div>
-          <div className="flex px-10 sm:px-20 md:px-0 max-w-[65rem] mx-auto justify-between items-center mt-20 lg:mt-24 child:text-white/60">
+          <div className="flex px-10 sm:px-20 md:px-0 max-w-[65rem] mx-auto justify-between items-center child:text-white/60">
             <div className="flex-center flex-col">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -461,7 +464,7 @@ export default function Landing({ info }) {
                 <path d="M403.802 334.499l-30.049-8.823-65.376-42.701v-9.784c18.928-14.277 31.926-36.1 34.342-61.115 9.493-1.528 16.766-9.777 16.766-19.693v-26.905c0-.104-.011-.206-.016-.31v-34.082a14.787 14.787 0 006.182-12.067v-31.19l45.117-7.685c7.415-1.263 12.747-7.152 13.271-14.655.522-7.503-3.941-14.075-11.108-16.354L260.834.757a15.914 15.914 0 00-9.698-.001L99.037 49.135c-7.167 2.279-11.631 8.851-11.108 16.353.332 4.767 2.61 8.877 6.126 11.569v40.787h-.372c-8.565 0-15.533 6.968-15.533 15.533v20.246l-10.027 58.852c-1.733 10.179 6.107 19.496 16.457 19.496h33.949c10.327 0 18.194-9.295 16.457-19.497l-10.027-58.851v-20.246c0-8.565-6.968-15.533-15.533-15.533h-.371V81.481l37.294 6.352v31.186c0 4.927 2.389 9.331 6.153 12.043l.014 34.419v26.902c0 9.917 7.273 18.165 16.766 19.693 2.415 25.001 15.399 46.814 34.309 61.092v9.83l-65.362 42.693-30.025 8.813c-32.129 9.43-54.815 38.927-54.815 73.215v22.352c0 4.142 3.357 7.5 7.5 7.5s7.5-3.357 7.5-7.5v-22.352c0-27.024 18.109-51.213 44.04-58.823l27.976-8.212 111.766 66.413c2.269 1.348 5.271 1.42 7.662 0l111.773-66.417 27.973 8.212c25.927 7.612 44.036 31.8 44.036 58.822v72.438c0 9.291-7.559 16.85-16.85 16.85h-33.057v-32.328a7.5 7.5 0 00-7.5-7.5 7.5 7.5 0 00-7.5 7.5v32.328H133.26v-32.328c0-4.142-3.357-7.5-7.5-7.5s-7.5 3.357-7.5 7.5v32.328H85.237c-9.291 0-16.85-7.559-16.85-16.85V465.13c0-4.142-3.357-7.5-7.5-7.5s-7.5 3.357-7.5 7.5v15.021c0 17.562 14.287 31.849 31.849 31.849h341.527c17.562 0 31.849-14.287 31.849-31.849v-72.438c0-33.48-21.912-63.555-54.81-73.214zM118.528 216.971H84.58a1.69 1.69 0 01-1.671-1.978l9.07-53.236h19.15l9.07 53.235a1.69 1.69 0 01-1.671 1.979zm-8.569-83.594v13.381H93.148v-13.381c0-.295.239-.534.534-.534h15.742a.533.533 0 01.535.534zm119.218 239.794l2.985 4.588-76.569-45.497 48.601-31.745c2.204 25.86 10.728 50.745 24.983 72.654zm-.052-32.375c17.653 2.508 36.087 2.51 53.75 0a143.248 143.248 0 01-12.624 24.196L256 386.896l-14.251-21.904a143.248 143.248 0 01-12.624-24.196zm53.698 32.375c14.255-21.909 22.779-46.794 24.982-72.654l48.601 31.744-76.568 45.498zm43.871-239.292h17.776v24.098h-8.823c-4.937 0-8.954-4.016-8.954-8.953v-15.145zm-223.111-70.45l152.101-48.38a.986.986 0 01.603.001l152.1 48.379c.314.1.745.236.69 1.017-.055.78-.5.855-.826.911l-50.288 8.565-18.88-2.335a7.5 7.5 0 00-1.84 14.886l13.41 1.659v30.748l-189.305-.007V88.131l77.607-9.603a138.926 138.926 0 0134.06 0l29.373 3.633a7.5 7.5 0 001.842-14.886l-29.373-3.633a153.855 153.855 0 00-37.743 0L154.02 73.924l-50.303-8.567c-.326-.056-.771-.131-.826-.911-.054-.781.377-.918.692-1.017zm63.928 94.548l-.01-24.098h17.805v15.146c0 4.936-4.016 8.953-8.954 8.953h-8.841zm16.507 50.244c-.386-6.107-5.462-10.891-11.555-10.891a4.954 4.954 0 01-4.949-4.948v-19.405h8.838c13.207 0 23.953-10.745 23.953-23.952v-15.146h111.389v15.146c0 13.207 10.746 23.952 23.953 23.952h8.838v19.405a4.954 4.954 0 01-4.949 4.948c-6.093 0-11.169 4.784-11.555 10.892-2.389 37.869-34.007 67.534-71.982 67.534s-69.592-29.665-71.981-67.535zM256 290.755a86.566 86.566 0 0037.378-8.473v4.753c0 12.823-1.73 25.472-5.061 37.672-20.382 3.886-43.425 4.043-64.634 0a142.926 142.926 0 01-5.061-37.672c0-.159-.022-.314-.032-.471-.002-.031.001-.062-.001-.093v-4.204A86.577 86.577 0 00256 290.755z"></path>
               </svg>
               <LandingCounter count={info.usersCount} />
-              <span className="text-2xl sm:text-3xl font-EstedadBold mt-2 sm:mt-4">
+              <span className="text-xl sm:text-2xl font-EstedadBold mt-2 sm:mt-4">
                 کاربر ثبت نام کردن
               </span>
             </div>
@@ -480,7 +483,7 @@ export default function Landing({ info }) {
                 <path d="M460.8 200.748V51.2c-.015-14.132-11.468-25.585-25.6-25.6h-8.533c-.015-14.132-11.468-25.585-25.6-25.6H256a33.918 33.918 0 00-25.6 11.796A33.918 33.918 0 00204.8 0H59.733c-14.132.015-25.585 11.468-25.6 25.6H25.6C11.468 25.615.015 37.068 0 51.2v315.733c.015 14.132 11.468 25.585 25.6 25.6h281.6V435.2c.049 42.395 34.405 76.751 76.8 76.8h51.2c42.395-.049 76.751-34.405 76.8-76.8V273.067c-.057-32.508-20.558-61.465-51.2-72.319zM426.667 42.667h8.533a8.544 8.544 0 018.533 8.533v145.57a76.766 76.766 0 00-8.533-.503h-8.533v-153.6zm-341.334-25.6h51.2v112.208l-18.938-23.671a8.528 8.528 0 00-13.324 0l-18.938 23.671V17.067zM51.2 25.6a8.544 8.544 0 018.533-8.533h8.533V153.6a8.53 8.53 0 0015.196 5.329l27.471-34.337 27.471 34.337a8.522 8.522 0 006.662 3.204 8.528 8.528 0 008.533-8.533V17.067h51.2c9.422.009 17.057 7.645 17.067 17.067v278.34a33.644 33.644 0 00-17.879-5.274H51.2V25.6zm0 298.667h152.788c9.391.329 17.019 7.693 17.678 17.067H51.2v-17.067zm256 51.2H25.6a8.544 8.544 0 01-8.533-8.533V51.2a8.544 8.544 0 018.533-8.533h8.533v307.2a8.533 8.533 0 008.533 8.533H307.2v17.067zm0-34.134h-68.066c.671-9.384 8.317-16.748 17.72-17.067H307.2v17.067zm0-34.133h-50.346a33.712 33.712 0 00-17.921 5.286V34.133c.009-9.422 7.645-17.057 17.067-17.067h145.067a8.544 8.544 0 018.533 8.533v170.667H384c-42.395.049-76.751 34.405-76.8 76.8V307.2zm93.867-93.867v110.933h-76.8v-51.2c.037-32.974 26.759-59.696 59.733-59.733h17.067zM494.933 435.2c-.037 32.974-26.759 59.696-59.733 59.733H384c-32.974-.037-59.696-26.759-59.733-59.733v-93.867h170.667V435.2zm0-110.933h-76.8V213.333H435.2c32.974.037 59.696 26.759 59.733 59.733v51.201z"></path>
               </svg>
               <LandingCounter count={info.coursesCount} />
-              <span className="text-2xl sm:text-3xl font-EstedadBold mt-2 sm:mt-4">
+              <span className="text-xl sm:text-2xl font-EstedadBold mt-2 sm:mt-4">
                 دوره آموزشی داریم
               </span>
             </div>
@@ -511,7 +514,7 @@ export default function Landing({ info }) {
                 </g>
               </svg>
               <LandingCounter count={info.totalTime} />
-              <span className="text-2xl sm:text-3xl font-EstedadBold mt-2 sm:mt-4">
+              <span className="text-xl sm:text-2xl font-EstedadBold mt-2 sm:mt-4">
                 دقیقه آموزش تولید کردیم
               </span>
             </div>
@@ -542,12 +545,6 @@ export default function Landing({ info }) {
         onClick={overlayOnClick}
         className={overlay ? "overlay overlay--visible" : "overlay"}
       ></div>
-      {/* <!-- Circle --> */}
-      <div className="circle circle--main circle--lg">
-        <div className="circle circle--md">
-          <div className="circle circle--sm"></div>
-        </div>
-      </div>
     </section>
   );
 }
