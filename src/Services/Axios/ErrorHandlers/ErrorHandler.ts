883abff3ed2f29c04eccc8cdf6a4948741ec5f8a
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-15 18:56:55 +0330

    Coding errorHandler Func for status 401 & 400

--- src/Services/Axios/ErrorHandlers/ErrorHandler.ts
+++ src/Services/Axios/ErrorHandlers/ErrorHandler.ts
@@ -1,13 +1,27 @@
-export const handleError = (error: any) => {
-    if (error.response) {
-      if (error.response?.status === 403) {
-        console.error("Unauthorized! Redirecting to login...");
-        // Redirecting to login page
-      }
-      console.error("Server Error:", error.response.data.message);
-    } else if (error.request) {
-      console.error("Network Error:", error.message);
-    } else {
-      console.error("Error:", error.message);
+import Swal from "sweetalert2";
+
+export const errorHandler = (error: any) => {
+  if (error.response) {
+    if (error.response?.status === 403) {
+      console.error("Unauthorized! Redirecting to login...");
+      // Redirecting to login page
+    } else if (error.response?.status === 401) {
+      Swal.fire({
+        title: "مقاله با این لینک از قبل منتشر شده! لطفا لینک مقاله رو عوض کن",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    } else if (error.response?.status === 400) {
+      Swal.fire({
+        title: "تمامی فیلد ها اجباری هستن",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
     }
-  };
+    console.error("Server Error:", error.response.data.message);
+  } else if (error.request) {
+    console.error("Network Error:", error.message);
+  } else {
+    console.error("Error:", error.message);
+  }
+};
