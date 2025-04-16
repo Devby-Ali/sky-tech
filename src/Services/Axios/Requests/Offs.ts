a60fc6207128b733dc53cbe3d964146f01bbd485
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-16 15:36:13 +0330

    Make Offs Service with Axios

---
+++ src/Services/Axios/Requests/Offs.ts
@@ -0,0 +1,43 @@
+import axiosInstance from "../Configs/axiosConfig";
+import { errorHandler } from "../ErrorHandlers/ErrorHandler";
+
+export const getOffs = async () => {
+  try {
+    const response = await axiosInstance.get("/offs");
+    return response.data;
+  } catch (error) {
+    errorHandler(error);
+    throw error;
+  }
+};
+
+export const createOff = async (newOffInfos: object) => {
+  try {
+    const response = await axiosInstance.post("/offs", newOffInfos);
+    return response;
+  } catch (error) {
+    errorHandler(error);
+    throw error;
+  }
+};
+
+export const createDiscount = async (reqBody: object) => {
+    try {
+        const response = await axiosInstance.post("/offs/all", reqBody)
+        console.log(response)
+        return response
+    } catch (error) {
+        errorHandler(error)
+        throw error
+    }
+}
+
+export const removeOff = async (offID: string) => {
+    try {
+        const response = await axiosInstance.delete(`/offs/${offID}`)
+        return response
+    } catch (error) {
+        errorHandler(error)
+        throw error
+    }
+}
